3a62432b253896c50af5114c37cbb79d08728f43
SanthoshKumar211299 <subjansan@gmail.com>
2025-06-20 20:10:58 +0530

    login and logout completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
-import User from "../models/user";
+import User from '../models/User.js'
 import bcrypt from 'bcryptjs';
-
+import jwt from 'jsonwebtoken'
 
 //register user: /api/user/register
 export const register = async (req,res)=> {
@@ -34,4 +34,68 @@ export const register = async (req,res)=> {
 
     }
 
+}
+
+//Login user: /api/user/login
+
+export const  login = async (req,res) => {
+    try{
+
+        const {email,password} = req.body;
+
+        if(!email ||!password)
+            return res.json({success: false, message:"Email and Password are Required!"});
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.json({success:false, message:'Invalid email or password'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch)
+             return res.json({success:false, message:'Invalid email or password'})
+
+        const token =jwt.sign({id:User._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+
+            res.cookie('token' , token, {
+                httpOnly:true, //prevent javascript to access cookie
+                secure: process.env.NODE_ENV ==='production' ,//Use secure cookies in production
+                sameSite:process.env.NODE_ENV ==='production' ? 'none' : 'strict',//CSPF PROTECTION
+                maxAge: 7 * 24 * 60 * 60 * 1000,//cookie expiring time
+            })
+          return res.json({success:true, user:{email:user.email,name:user.name}})
+
+    }catch(error){
+        console.log(error.message);
+        res.json({sucess:false,message: error.message})
+    }
+}
+
+//Check Auth: /api/user/is-auth
+
+export const isAuth = async (req, res)=> {
+  try {
+      const { userId } = req.body;
+      const user = await User.findById(userId).select("-password")
+      return res.json({success:true, user})
+  } catch (error) {
+    console.log(error.message);
+    res.json({sucess:false,message: error.message})
+  }
+}
+
+//Logout user : /api/user/logout
+
+export const logout = async (req,res) =>{
+  try {
+    res.clearCookie("token", {
+        httpOnly:true,
+        secure:process.env.NODE_ENV = 'production',
+        sameSite: process.env.NODE_ENV = 'production' ? 'none' : 'strict',
+    })
+    return res.json({success:true,message:"Logged Out"})
+  } catch (error) {
+     console.log(error.message);
+    res.json({sucess:false,message: error.message})
+  }
 }
--- server/models/user.js
+++ server/models/user.js
@@ -10,14 +10,3 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.models.user || mongoose.model('user', userSchema)
 
 export default User;
-
-/*const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique:true},
-    password: {type: String, required: true},
-    cartItems: {type: String,default:{}},
-},{minimize:false})
-
-const User = mongoose.models.user || mongoose.model('user', userSchema)
-
-export default User;*/
